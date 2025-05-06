e52b36a4ebd75f1248c8352b66e018e67b52b379
Naman <naman.chachan@mitwpu.edu.in>
2025-05-06 22:51:14 +0530

    203

---
+++ LinkedList/203.cpp
@@ -0,0 +1,36 @@
+//203. Remove Linked List Elements
+//https://leetcode.com/problems/remove-linked-list-elements/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* removeElements(ListNode* head, int val) {
+            ListNode* dummy = new ListNode();
+            dummy->next = head;
+    
+            ListNode* prev = dummy;
+            ListNode* curr = head;
+    
+            while(curr){
+                if(curr->val == val){
+                    prev->next = curr->next;
+                    delete curr;
+                    curr = prev->next;
+                }
+                else{
+                    prev = curr;
+                    curr = curr->next;
+                }
+            }
+            return dummy->next;
+        }
+    };
