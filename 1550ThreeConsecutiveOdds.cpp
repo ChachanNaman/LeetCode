bf8144f755131dd8b523f2778051d0b546c7a34f
Naman <naman.chachan@mitwpu.edu.in>
2025-05-12 13:38:58 +0530

    1550

---
+++ 1550ThreeConsecutiveOdds.cpp
@@ -0,0 +1,15 @@
+//1550. Three Consecutive Odds
+//https://leetcode.com/problems/three-consecutive-odds/description/
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        for (int i = 0; i < n - 2; i++) {
+            if (arr[i] % 2 && arr[i + 1] % 2 && arr[i + 2] % 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
